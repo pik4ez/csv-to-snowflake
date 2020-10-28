755e6e5a2b12b3575d778921f3d0d54e2a9cacc9
Dmitry Belov <pik4ez@gmail.com>
2020-10-28 10:39:12 +0200

    Added CSV delimiter and line terminator options. Version bumped to 0.2.0.

--- csv_to_snowflake/main.py
+++ csv_to_snowflake/main.py
@@ -26,24 +26,31 @@ def run():
     parser = argparse.ArgumentParser(
         description='Creates Snowflake table from CSV file.')
     parser.add_argument('--log-level', default="warning", help='Snowflake user')
+    parser.add_argument('--debug', default=False,
+                        type=bool, help='Enable debug mode')
     parser.add_argument(
         '--account', required=True,
-        help='Snowflake account. For example, mf00000.eu-central-1')
-    parser.add_argument('--user', required=True, help='Snowflake user')
-    parser.add_argument('--role', required=True, help='Snowflake role')
-    parser.add_argument('--db', required=True, help='Snowflake database')
-    parser.add_argument('--schema', required=True, help='Snowflake schema')
-    parser.add_argument('--table', required=True, help='Snowflake table')
-    parser.add_argument('csv_file', type=str, help='Path to CSV file')
+        help='Snowflake account. For example, mf00000.eu-central-1.')
+    parser.add_argument('--user', required=True, help='Snowflake user.')
+    parser.add_argument('--role', required=True, help='Snowflake role.')
+    parser.add_argument('--db', required=True, help='Snowflake database.')
+    parser.add_argument('--schema', required=True, help='Snowflake schema.')
+    parser.add_argument('--table', required=True, help='Snowflake table.')
+    parser.add_argument('--csv-delimiter', default=',', help='CSV delimiter.')
+    parser.add_argument('--csv-lineterminator',
+                        default='\r\n', help='CSV line terminator.')
+    parser.add_argument('csv_file', type=str, help='Path to CSV file.')
 
     args = parser.parse_args()
 
     cur_log_level = log_levels.get(args.log_level)
-    print("setting log level", cur_log_level)
     logger.setLevel(cur_log_level)
 
     with open(args.csv_file) as csv_file:
-        reader = csv.DictReader(csv_file)
+        csv.register_dialect(
+            'dialect', delimiter=args.csv_delimiter,
+            lineterminator=args.csv_lineterminator)
+        reader = csv.DictReader(csv_file, dialect='dialect')
         columns = reader.fieldnames
 
         ctx = snowflake.connector.connect(
@@ -56,6 +63,7 @@ def run():
         cs = ctx.cursor()
 
         csv_abs_path = Path(args.csv_file).resolve()
+        csv_filename = ntpath.basename(csv_abs_path)
 
         try:
             sql_use_schema = f"USE SCHEMA {args.db}.{args.schema}"
@@ -73,17 +81,31 @@ def run():
             logger.debug(sql_put)
             cs.execute(sql_put)
 
-            if args.log_level == "debug":
+            if args.debug == True:
                 sql_list = f"LIST {stage}"
                 logger.debug(sql_list)
                 res = cs.execute(sql_list).fetchall()
                 for r in res:
                     logger.debug(r)
 
-            csv_filename = ntpath.basename(csv_abs_path)
-            sql_copy = f"""COPY INTO {args.table} FROM '{stage}'
+                import gzip
+                import tempfile
+                with tempfile.TemporaryDirectory() as tmp_dir:
+                    sql_get = f"GET {stage}/{csv_filename} file://{tmp_dir}"
+                    logger.debug(sql_get)
+                    cs.execute(sql_get)
+                    tmp_filepath = f"{tmp_dir}/{csv_filename}.gz"
+                    with open(tmp_filepath, 'rb') as f:
+                        logger.debug("gunzipped stage file", f.read())
+
+            sql_copy = f"""COPY INTO {args.table}
+            FROM '{stage}'
             FILES = ('{csv_filename}.gz')
-            FILE_FORMAT = (TYPE=CSV, COMPRESSION=GZIP);"""
+            FILE_FORMAT = (
+                TYPE=CSV,
+                COMPRESSION=GZIP,
+                FIELD_DELIMITER='{args.csv_delimiter}'
+                RECORD_DELIMITER='{args.csv_lineterminator}');"""
             logger.debug(sql_copy)
             cs.execute(sql_copy)
 
